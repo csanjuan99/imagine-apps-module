efc9058c1aef2e029807b497ac0196c4a738f20f
Carlos Sanjuan <carlosdsanjuan99@icloud.com>
2024-02-07 18:18:14 -0500

    Update cors

--- repository/UserRepository.py
+++ repository/UserRepository.py
@@ -1,9 +1,8 @@
 import models.User as User
-from schemas.User import User as UserModel
 from sqlalchemy.orm import Session
 
 
-def get_user_by_email(db: Session, email: str) -> UserModel:
+def get_user_by_email(db: Session, email: str):
     return db.query(User).filter(User.email == email).first()
 
 
